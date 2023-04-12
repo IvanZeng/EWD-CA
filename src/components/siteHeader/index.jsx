51cc27c8845bbc7ebf89c2c4efa83453b25cc391
Qi Zeng <ivan_zq@foxmail.com>
2023-04-12 20:03:18 +0100

    CA-Bug fix- drop down menu can not go to position.

--- src/components/siteHeader/index.jsx
+++ src/components/siteHeader/index.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { styled } from "@mui/material/styles";
 import MenuItem from "@mui/material/MenuItem";
-import { useNavigate, useLocation  } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Select from "@mui/material/Select";
 import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
 
 const styles = {
   title: {
@@ -24,11 +23,10 @@ const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
 const SiteHeader = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const [anchorEl, setAnchorEl] = useState(null);
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
-  const location = useLocation();
-  const [selectedMenu, setSelectedMenu] = useState(location.pathname);
 
   const menuOptions = [
     { label: "Home", path: "/" },
@@ -40,6 +38,18 @@ const SiteHeader = () => {
     { label: "Actors", path: "/actors" },
     { label: "Favorite Actors", path: "/actors/favourites" },
   ];
+
+  const isPathInMenuOptions = menuOptions.some(
+    (option) => option.path === location.pathname
+  );
+
+  const [selectedMenu, setSelectedMenu] = useState(
+    isPathInMenuOptions ? location.pathname : "Menu"
+  );
+
+  useEffect(() => {
+    setSelectedMenu(isPathInMenuOptions ? location.pathname : "Menu");
+  }, [location.pathname, isPathInMenuOptions]);
 
   const handleMenuSelect = (pageURL) => {
     navigate(pageURL);
@@ -55,45 +65,24 @@ const SiteHeader = () => {
           <Typography variant="h6" sx={styles.title}>
             All you ever wanted to know about Movies!
           </Typography>
-          {isMobile ? (
-            <>
-              <FormControl>
-                <Select
-                  id="menu-select"
-                  value={selectedMenu}
-                  onChange={(e) => {
-                    setSelectedMenu(e.target.value);
-                    handleMenuSelect(e.target.value);
-                  }}
-                >
-                  {menuOptions.map((opt) => (
-                    <MenuItem key={opt.label} value={opt.path}>
-                      {opt.label}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </>
-          ) : (
-            <>
-              <FormControl>
-                <Select
-                  id="menu-select"
-                  value={selectedMenu}
-                  onChange={(e) => {
-                    setSelectedMenu(e.target.value);
-                    handleMenuSelect(e.target.value);
-                  }}
-                >
-                  {menuOptions.map((opt) => (
-                    <MenuItem key={opt.label} value={opt.path}>
-                      {opt.label}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </>
-          )}
+          <FormControl>
+            <Select
+              id="menu-select"
+              value={selectedMenu}
+              onChange={(e) => {
+                handleMenuSelect(e.target.value);
+              }}
+            >
+              <MenuItem value="Menu" disabled>
+                Menu
+              </MenuItem>
+              {menuOptions.map((opt) => (
+                <MenuItem key={opt.label} value={opt.path}>
+                  {opt.label}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
         </Toolbar>
       </AppBar>
       <Offset />
