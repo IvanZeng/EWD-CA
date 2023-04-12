8849cd7ae2fba230114d37409b04aa4badfeec1c
Qi Zeng <ivan_zq@foxmail.com>
2023-04-12 16:29:01 +0100

    CA-Get more actors in Actor list from request results.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -211,16 +211,31 @@ export const getSimilarMovies = (id) =>
      });
     };
 
-    export const getActors = () => {
-      return fetch(
-        `https://api.themoviedb.org/3/person/popular?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=1`
-      ).then((response) => {
-        if (!response.ok) {
-          throw new Error(response.json().message);
+    export const getActors = async () => {
+      const totalPages = 5; // Set this to the number of pages you want to fetch (5 pages * 20 results per page = 100 actors)
+      const allActors = [];
+    
+      try {
+        for (let page = 1; page <= totalPages; page++) {
+          const response = await fetch(
+            `https://api.themoviedb.org/3/person/popular?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&page=${page}`
+          );
+    
+          if (!response.ok) {
+            throw new Error((await response.json()).message);
+          }
+    
+          const data = await response.json();
+          allActors.push(...data.results);
         }
-        return response.json();
-      })
-      .catch((error) => {
-         throw error
-      });
+      } catch (error) {
+        throw error;
+      }
+    
+      return {
+        page: 1,
+        results: allActors,
+        total_pages: 1,
+        total_results: allActors.length,
+      };
     };
